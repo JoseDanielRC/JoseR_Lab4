a6183631ef8aaee5e8e9a8c099b24ee86dbfa2fc
Daniel Rodriguez <josedanielrc@unitec.edu>
2019-05-10 14:17:45 -0600

    Se inicializa la matriz y se llena el primer renglon con el string

--- JoseR_Lab4.cpp
+++ JoseR_Lab4.cpp
@@ -4,8 +4,22 @@
 using std::cout;
 using std::cin;
 using std::endl;
-
+using std::string;
 int main(){
-
-
+	string fila;
+	int largo=0;
+	int ancho=fila.length();
+	cout<<"Ingrese la primera fila de caracteres"<<endl;
+	cin>>fila;
+	cout<<"Ingrese la cantidad de filas"<<endl;
+	cin>>largo;
+        int** matriz=NULL;
+        matriz= new int*[fila.length()];
+        for(int i=0;i<largo;i++){
+                matriz[i]=new int*[largo];
+                }
+	for(int i=0;i<fila.length();i++){
+                matriz[i]=fila[i];
+                }
+	
 	}
