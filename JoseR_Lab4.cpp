d400ee4744e0766d728338bf6ada9e79137818f6
Daniel Rodriguez <josedanielrc@unitec.edu>
2019-05-10 16:25:16 -0600

    Probando la matriz resultante

--- JoseR_Lab4.cpp
+++ JoseR_Lab4.cpp
@@ -17,38 +17,46 @@ for(int i=0;i<largo;i++){
 int main(){
 	string fila;
 	int largo=0;
-	int ancho=fila.length();
+	int ancho;
+	int contlibre=0,contramp=0;
 	cout<<"Ingrese la primera fila de caracteres"<<endl;
 	cin>>fila;
 	cout<<"Ingrese la cantidad de filas"<<endl;
 	cin>>largo;
+	ancho = fila.length();
 	stringstream filanterior;
         char** matriz=NULL;
-        matriz= new char*[fila.length()];
-        for(int i=0;i<ancho;i++){
-                matriz[i]=new char[largo];
+        matriz= new char*[largo];
+        for(int i=0;i<largo;i++){
+                matriz[i]=new char[ancho];
                 }
-	for(int i=0;i<fila.length();i++){
-                matriz[0][i]=fila[i];
+	for(int i=0;i<ancho;i++){
+              matriz[0][i]=fila[i];
                 }
 	for(int i=1;i<largo;i++){
 		for(int j=0;j<ancho;j++){
 			if(j==0){
 			filanterior<<'.'<<matriz[i-1][j]<<matriz[i-1][j+1];
 
-			}else if(j==9){
+			}else if(j==ancho-1){
                         filanterior<<matriz[i-1][j-1]<<matriz[i-1][j]<<'.';
 
                         }else{
 			filanterior<<matriz[i-1][j-1]<<matriz[i-1][j]<<matriz[i-1][j+1];
 			}	
-        	    	if(filanterior.str()=="^^."&&filanterior.str()==".^^"&&filanterior.str()=="^.."&&filanterior.str()=="..^"){
+        	    	if(filanterior.str()=="^^."|| filanterior.str()==".^^"|| filanterior.str()=="^.."|| filanterior.str()=="..^"){
 			matriz[i][j]='^';
+			filanterior.str("");
+			contramp++;
 			}else{
 				matriz[i][j]='.';
+				contlibre++;
+				filanterior.str("");
+
 			}
-		filanterior.str()="";	
+			
                	 }
          }
 	imprimir(matriz,largo,ancho);
+	cout<<"libres: "<<contlibre<<"trampas: "<<filanterior<<endl;
 }
