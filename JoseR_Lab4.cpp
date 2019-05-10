5a2dbcf502681d46f9ed0085162da9f5f5a99e65
Daniel Rodriguez <josedanielrc@unitec.edu>
2019-05-10 14:51:57 -0600

    Iniciando a llenar la matriz

--- JoseR_Lab4.cpp
+++ JoseR_Lab4.cpp
@@ -1,10 +1,11 @@
-
+#include <sstream>
 #include<iostream>
 #include<string>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::stringstream;
 int main(){
 	string fila;
 	int largo=0;
@@ -13,13 +14,23 @@ int main(){
 	cin>>fila;
 	cout<<"Ingrese la cantidad de filas"<<endl;
 	cin>>largo;
-        int** matriz=NULL;
-        matriz= new int*[fila.length()];
-        for(int i=0;i<largo;i++){
-                matriz[i]=new int*[largo];
+	stringstream filanterior;
+        char** matriz=NULL;
+        matriz= new char*[fila.length()];
+        for(int i=0;i<ancho;i++){
+                matriz[i]=new char[largo];
                 }
 	for(int i=0;i<fila.length();i++){
-                matriz[i]=fila[i];
+                matriz[0][i]=fila[i];
                 }
-	
-	}
+	for(int i=0;i<largo;i++){
+		for(int j=0;j<ancho;j++){
+		filanterior<<matriz[i-1][j]<<matriz[i-1][j-1]<<matriz[i-1][j+1];	
+            	if(filanterior.str()=="^^."&&filanterior.str()==".^^"&&filanterior.str()=="^.."&&filanterior.str()=="..^"){
+			matriz[i][j]='^';
+			}else{
+				matriz[][]='.';
+			}	
+               	 }
+         }
+}
